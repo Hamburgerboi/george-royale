ecc33ee93483a0e9daec1ebb3091b6b2f1222d5a
crab_eats_onions <45152909+Hamburgerboi@users.noreply.github.com>
2020-09-12 10:32:50 -0700

    Update 1.54
    Changelog
    
    - Fixed bug that caused Towers to move position
    - Fixed bug that would cause players to respawn after tower destroyed
    - Now Network-Destroys towers instead of Instance-Destroy

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,31 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class Tower : MonoBehaviour
+public class Tower : MonoBehaviourPun
 {
     [Header("Health")]
     public float TowerHealth = 1000f;
     private float currentTowerHealth;
 
     [Header("Animations")]
-    public float shakeDuration = 1.0f;
+    public float shakeDuration = 0.3f;
+    public float shakeMultiplier = 0.1f;
 
+    private float frameCount = 0;
     private float currentShakeTime = 0f;
+    private Vector3 defaultLocation;
 
     void Start()
     {
         currentTowerHealth = TowerHealth;
+        defaultLocation = transform.position;
     }
 
     void Update()
     {
+        frameCount++;
         if(currentShakeTime > 0)
         {
-            Vector3 pos = Random.insideUnitCircle * 5;
+            Vector3 pos = Random.insideUnitCircle * shakeMultiplier;
             transform.position += pos;
             currentShakeTime -= Time.deltaTime;
         }
+    }
+
+    void LateUpdate()
+    {
+        if(frameCount % 2 == 0) transform.position = defaultLocation;
     }
 
     public void Damage(float amount)
@@ -41,6 +52,6 @@ public class Tower : MonoBehaviour
 
     private void DestoryTower()
     {
-        Destroy(gameObject);
+        if(photonView.IsMine) PhotonNetwork.Destroy(gameObject);
     }
 }
